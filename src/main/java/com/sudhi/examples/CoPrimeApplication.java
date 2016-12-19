6ff8c0a3d50d92f5bd777a5eae5a9090b27bec6d
sudhi_bhat <sudhi.bhat79@gmail.com>
2016-12-19 19:24:25 +0530

    Initial Commit

---
+++ src/main/java/com/sudhi/examples/CoPrimeApplication.java
@@ -0,0 +1,12 @@
+package com.sudhi.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoPrimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoPrimeApplication.class, args);
+	}
+}
